f55fd241c1153de266897e4e8183e306a9221ff4
Othman Semlali <othmansemlali1@gmail.com>
2024-04-24 15:09:48 +0100

    changes

--- Front/src/Utils/helpers.js
+++ Front/src/Utils/helpers.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const formatPrice = (number) => {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
@@ -5,3 +7,24 @@ export const formatPrice = (number) => {
     }).format(number);
   };
   
+
+//   export function useDebounce(value, delay) {
+//     // State and setters for debounced value
+//     const [debouncedValue, setDebouncedValue] = useState(value);
+  
+//     useEffect(() => {
+//       // Update debounced value after delay
+//       const handler = setTimeout(() => {
+//         setDebouncedValue(value);
+//       }, delay);
+  
+//       // Cancel the timeout if value changes (also on delay change or unmount)
+//       // This is how we prevent debounced value from updating if value is changed ...
+//       // .. within the delay period. Timeout gets cleared and restarted.
+//       return () => {
+//         clearTimeout(handler);
+//       };
+//     }, [value, delay]); // Only re-call effect if value or delay changes
+  
+//     return debouncedValue;
+//   }
