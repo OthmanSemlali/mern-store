c6e46fcac81f90180dda452d9f29ddc6783863e3
Othman Semlali <othmansemlali1@gmail.com>
2024-05-11 10:25:00 +0100

    init back-office project

---
+++ back-office/src/pages/Error/notFound.jsx
@@ -0,0 +1,11 @@
+export function NotFound(){
+
+
+    return (
+        <div className="bg-red-200 h-screen absolute h-full w-full">
+            ROUTE NOT FOUND
+        </div>
+    )
+}
+
+export default NotFound;
